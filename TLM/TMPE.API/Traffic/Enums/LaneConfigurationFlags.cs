68af5e03fdaa3b8f7e65f9f46ab62b155c56f2a9
Elesbaan70 <95266227+Elesbaan70@users.noreply.github.com>
2022-03-05 15:30:35 -0600

    enum and UI elements for lane configuration flags

---
+++ TLM/TMPE.API/Traffic/Enums/LaneConfigurationFlags.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficManager.API.Traffic.Enums {
+    public enum LaneConfigurationFlags {
+        None = 0,
+        CrossLeft = 1,
+        CrossRight = 1 << 1,
+        TurnOutOfDisplaced = 1 << 2,
+        TurnIntoDisplaced = 1 << 3,
+        ForwardDisplaced = 1 << 4,
+
+        Computed = CrossLeft | CrossRight | TurnOutOfDisplaced | TurnIntoDisplaced | ForwardDisplaced,
+    }
+}
